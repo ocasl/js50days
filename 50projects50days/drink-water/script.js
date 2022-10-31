544a01584543000697caf3da1901599720e724f7
dddd <241568260@qq.com>
2022-10-31 19:46:04 +0800

    ddd

--- 50projects50days/drink-water/script.js
+++ 50projects50days/drink-water/script.js
@@ -1,10 +1,14 @@
 const smallCups = document.querySelectorAll('.cup-small')
+
 const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
 updateBigCup()
+ 
 
+
+//  根据idx 来给瓶子上full
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
@@ -14,7 +18,7 @@ function highlightCups(idx) {
     else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
         idx--
     }
-
+//   如果我点击的瓶子小于idx值那么我一掉这个瓶子的full
     smallCups.forEach((cup, idx2) => {
         if(idx2 <= idx) {
             cup.classList.add('full')
@@ -27,9 +31,11 @@ function highlightCups(idx) {
 }
 
 function updateBigCup() {
+    //  get all fullCups
     const fullCups = document.querySelectorAll('.cup-small.full').length
+    // get all totalCups
     const totalCups = smallCups.length
-
+//  if fullcups  = 0  then  percentage   miss
     if(fullCups === 0) {
         percentage.style.visibility = 'hidden'
         percentage.style.height = 0
