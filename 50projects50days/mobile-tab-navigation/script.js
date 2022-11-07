e8cf435e5e050af61262e3ab417e7eea9ae7472b
dddd <241568260@qq.com>
2022-11-07 23:59:06 +0800

    ddd

--- 50projects50days/mobile-tab-navigation/script.js
+++ 50projects50days/mobile-tab-navigation/script.js
@@ -3,19 +3,13 @@ const listItems = document.querySelectorAll('nav ul li')
 
 listItems.forEach((item, idx) => {
     item.addEventListener('click', () => {
-        hideAllContents()
-        hideAllItems()
+        //  先全部移除  移除之后再打开点了的这个
+        contents.forEach(content => content.classList.remove('show'))
+
+        listItems.forEach(item => item.classList.remove('active'))
 
         item.classList.add('active')
         contents[idx].classList.add('show')
     })
 })
 
-function hideAllContents() {
-    contents.forEach(content => content.classList.remove('show'))
-}
-
-
-function hideAllItems() {
-    listItems.forEach(item => item.classList.remove('active'))
-}
